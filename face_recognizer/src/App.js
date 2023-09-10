fbae64f3d2bd7b3d3ab7a6a490ce55969dfe655a
Fab-c <71762204+fab-c14@users.noreply.github.com>
2023-09-10 13:11:56 +0000

    Day 3 2nd Commit

--- face_recognizer/src/App.js
+++ face_recognizer/src/App.js
@@ -1,16 +1,39 @@
 import React, {Component} from "react";
+// import Particles from "react-tsparticles-js"; 
 
+import "tachyons";
 import Navigation from "./components/Navigation";
 import Logo from "./components/Logo/Logo"
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Rank from './components/Rank/Rank';
+import './App.css'
+import Particles from 'react-particles-js';
+
+const particleOptions = {
+  particles:{
+    
+      number:{
+         value:30,
+         density:{
+          enable:true,
+          value_area:800
+         }
+    }
+  }
+}
 class App extends Component{
   render(){
+
     return(
       <div className="App">
+       <Particles height="100vh" width="100vw" params={particleOptions} />
+
         <Navigation/>
         <Logo />
+        <Rank />
+        <ImageLinkForm/>
         {/* 
 
-        <ImageLinkForm/>
         <FaceRecognition /> */}
       </div>
     )
--- face_recognizer/src/components/Logo/Logo.js
+++ face_recognizer/src/components/Logo/Logo.js
@@ -5,8 +5,8 @@ import "./Logo.css";
 const Logo = ()=>{
     return (
         <div className='ma4 mt0 tc'>
-            <Tilt className="Tilt br2 shadow-2" options={{max:55}} style={{height:100,width:120}}>
-                <div className='Tilt-inner pa3'><FaBeer style={{paddingTop:'7px'}}/></div>
+            <Tilt className="Tilt br2 shadow-2" options={{max:70}} style={{height:100,width:120}}>
+                <div className='Tilt-inner pa3'><FaBeer style={{paddingTop:'5px'}} size={45}/></div>
             </Tilt>        
         </div>
     );
