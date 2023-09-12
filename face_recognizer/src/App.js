d70e70786bb844ff780bfc3ce855119ddf96cd45
Fab-c <71762204+fab-c14@users.noreply.github.com>
2023-09-12 14:41:50 +0000

    Day 5 First commit

--- face_recognizer/src/App.js
+++ face_recognizer/src/App.js
@@ -1,5 +1,3 @@
-
-// import Particles from "react-tsparticles-js"; 
 import React,{Component} from "react";
 import "tachyons";
 import Navigation from "./components/Navigation";
@@ -7,35 +5,19 @@ import Logo from "./components/Logo/Logo"
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Rank from './components/Rank/Rank';
 import './App.css';
-import Particles from 'react-tsparticles';
+
+
 
 
 
 
 class App extends Component{
   render(){
+    
 
     return(
       <div className="App">
-        <Particles
-            params={{
-              "particles": {
-                  "number": {
-                      "value": 100
-                  },
-                  "size": {
-                      "value": 45
-                  }
-              },
-              "interactivity": {
-                  "events": {
-                      "onhover": {
-                          "enable": true,
-                          "mode": "repulse"
-                      }
-                  }
-              }
-          }} />
+
         <Navigation/>
         <Logo />
         <Rank />
@@ -44,6 +26,7 @@ class App extends Component{
 
         <FaceRecognition /> 
         */}
+        
       </div>
     )
   }
